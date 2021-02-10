5484de28e4e9aa731fbb1da3554c2aad88b7bc16
Petteri Tolonen <petteri.tolonen@cybercom.com>
2021-02-10 17:01:40 +0200

    KTO-1101: Disabling individual radio buttons in RadioGroup
    - Use either disabled-attribute on RadioGroup-children or getIsDisabled-attribute to adjust, which individual radio-buttons in RadioGroup are disabled.

--- src/RadioGroup/RadioGroup.test.tsx
+++ src/RadioGroup/RadioGroup.test.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { screen } from '@testing-library/react';
 import { render } from '../testUtils';
 import Radio from '../Radio';
 import RadioGroup from './index';
@@ -12,4 +13,78 @@ it('Renders a RadioGroup with 3 radio buttons', () => {
     </RadioGroup>,
   );
   expect(rootElement).toMatchSnapshot();
+});
+
+it('Renders individual radio buttons disabled using disabled-attribute', () => {
+  render(
+    <RadioGroup value="b" onChange={() => {}}>
+      <Radio disabled={true} value="a">
+        Radio A
+      </Radio>
+      <Radio value="b">Radio B</Radio>
+      <Radio value="c">Radio C</Radio>
+    </RadioGroup>,
+  );
+
+  expect(screen.getByLabelText('Radio A')).toHaveAttribute('disabled');
+  expect(screen.getByLabelText('Radio B')).not.toHaveAttribute('disabled');
+  expect(screen.getByLabelText('Radio C')).not.toHaveAttribute('disabled');
+});
+
+it('Renders individual radio buttons disabled using getIsDisabled-attribute', () => {
+  render(
+    <RadioGroup
+      value="b"
+      onChange={() => {}}
+      getIsDisabled={(value) => value === 'a'}
+    >
+      <Radio value="a">Radio A</Radio>
+      <Radio value="b">Radio B</Radio>
+      <Radio value="c">Radio C</Radio>
+    </RadioGroup>,
+  );
+
+  expect(screen.getByLabelText('Radio A')).toHaveAttribute('disabled');
+  expect(screen.getByLabelText('Radio B')).not.toHaveAttribute('disabled');
+  expect(screen.getByLabelText('Radio C')).not.toHaveAttribute('disabled');
+});
+
+it('Renders individual radio buttons disabled using getIsDisabled-attribute (options-attribute)', () => {
+  render(
+    <RadioGroup
+      value="b"
+      onChange={() => {}}
+      options={[
+        { label: 'Radio A', value: 'a' },
+        { label: 'Radio B', value: 'b' },
+        { label: 'Radio C', value: 'c' },
+      ]}
+      getIsDisabled={(value) => value === 'a'}
+    />,
+  );
+
+  expect(screen.getByLabelText('Radio A')).toHaveAttribute('disabled');
+  expect(screen.getByLabelText('Radio B')).not.toHaveAttribute('disabled');
+  expect(screen.getByLabelText('Radio C')).not.toHaveAttribute('disabled');
+});
+
+it('Renders all radio buttons disabled when RadioGroup disabled-attibute is true', () => {
+  render(
+    <RadioGroup
+      value="b"
+      disabled={true}
+      onChange={() => {}}
+      getIsDisabled={(value) => value === 'a'}
+    >
+      <Radio value="a">Radio A</Radio>
+      <Radio disabled={true} value="b">
+        Radio B
+      </Radio>
+      <Radio value="c">Radio C</Radio>
+    </RadioGroup>,
+  );
+
+  expect(screen.getByLabelText('Radio A')).toHaveAttribute('disabled');
+  expect(screen.getByLabelText('Radio B')).toHaveAttribute('disabled');
+  expect(screen.getByLabelText('Radio C')).toHaveAttribute('disabled');
 });
