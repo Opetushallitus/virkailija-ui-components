1279a2488c70ea18aab214e378ed1f85ad45110a
Tatu Honkanen <tatu.honkanen@cybercom.com>
2021-03-22 15:30:58 +0200

    OY-2817 : Updated storyboard 5.3.21 -> 6.1.21

--- .storybook/main.js
+++ .storybook/main.js
@@ -2,7 +2,6 @@ const path = require('path');
 
 module.exports = {
   stories: ['../src/**/*.stories.tsx'],
-  presets: ['@storybook/preset-typescript'],
   addons: [
     '@storybook/addon-actions/register',
     '@storybook/addon-links/register',
